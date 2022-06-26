9f89a853bfd88d93b805e412256f33ec28b431d9
Mitchell Stahl <mitchellstahl@Mitchells-MBP.attlocal.net>
2022-06-25 20:35:50 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('#hamburger');
+const header = document.querySelector(".header");
+const list = document.querySelector("#list");
+
+hamburger.addEventListener('click', function () {
+
+    alert("roy is coming");
+
+
+})
