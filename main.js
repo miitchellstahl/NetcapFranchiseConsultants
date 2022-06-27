dbe2176a945e3fd4d9d7e8977faaa54fc68d1dc2
Mitchell Stahl <mitchellstahl@Mitchells-MBP.attlocal.net>
2022-06-27 17:39:26 -0400

    mobile-nav-first

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector(".header");
 const list = document.querySelector("#list");
+const mobile_menu = document.querySelector(".mobile-nav");
+let width = window.innerWidth;
 
 hamburger.addEventListener('click', function () {
 
-    alert("roy");
+    this.classList.toggle("is-active");
 
 })
