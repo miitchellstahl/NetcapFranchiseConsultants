b0cfd680da76baf5ed85b0543588476fe9a62c0b
Mitchell Stahl <mitchellstahl@Mitchells-MBP.attlocal.net>
2022-06-28 01:26:45 -0400

    mobile-nav-menu-one

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector(".header");
-const list = document.querySelector("#list");
-const mobile_menu = document.querySelector(".mobile-nav");
+const mobileNav = document.querySelector(".mobile-nav");
+const main = document.querySelector(".main");
 let newWidth = document.documentElement.clientWidth;
 let newHeight = document.documentElement.clientHeight;
 let lastScrollY = window.scrollY;
 
 hamburger.addEventListener('click', function () {
 
-    this.classList.toggle("is-active");
+    mobileNav.classList.toggle("is-active");
+    hamburger.classList.toggle("is-active");
+    main.classList.toggle("is-active");
 
 })
 
 window.addEventListener('resize', function (window) {
     newWidth = document.documentElement.clientWidth;
     newHeight = document.documentElement.clientHeight;
+    if (newWidth > 850) {
+        mobileNav.classList.remove("is-active");
+        main.classList.remove("is-active");
+        hamburger.classList.remove("is-active");
+    }
 });
 
-window.addEventListener('scroll', () => {
 
-    console.log(lastScrollY);
 
-    if (lastScrollY < window.scrollY && newWidth < 850 && lastScrollY > 100) {
-        header.classList.add("nav--hidden");
-    }
-    else {
-        header.classList.remove("nav--hidden");
-    }
 
-    lastScrollY = window.scrollY;
-})
+
+// window.addEventListener('scroll', () => {
+
+//     console.log(lastScrollY);
+
+//     if (lastScrollY < window.scrollY && newWidth < 850 && lastScrollY > 100) {
+//         header.classList.add("nav--hidden");
+//     }
+//     else {
+//         header.classList.remove("nav--hidden");
+//     }
+
+//     lastScrollY = window.scrollY;
+// })
